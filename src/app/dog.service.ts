cad6682cc7ca0a336b3bf6d86ed25a567a60f744
williamfernandessilva <willgsvs@hotmail.com>
2023-04-25 16:59:02 -0300

    25/04

---
+++ src/app/dog.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {Dog} from './dog';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DogService {
+  constructor(private http: HttpClient  ) {  }
+
+  getDog() : Observable<Dog> {
+      return this.http.get<Dog>("https://dog.ceo/api/breeds/image/random")
+  }
+}
